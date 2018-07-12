c0789272ddfef8eb5a0c1b76ce51d8d6ff831842
Juan D. Bolanos <bolanosjd@gmail.com>
2018-07-12 17:37:00 -0400

    npm dependencies added

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// Requires express.js from node modules
+const express = require('express');
+
+// Creates express application 
+const app = express();
+
+// Create a route
+app.get('/', (req, res) => {
+    res.send("What up world, I'm alive!");
+});
+
+app.get('/hello', (req, res) => {
+    res.send("<h1>What up world, I'm tagged!</h1>");
+});
+
+// Sets up development server
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000');
+});
