80d780606db5429c4923ada4f481ebae1c0bab80
Juan D. Bolanos <bolanosjd@gmail.com>
2018-07-16 14:29:20 -0400

    Request and Response object learning

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 // Requires express.js from node modules
 const express = require('express');
 
+const bodyParser = require('body-parser');
 // Creates express application 
 const app = express();
 
 // Set pug on view engine
+app.use(bodyParser.urlencoded({ extended: false}));
 app.set('view engine', 'pug');
 
-// Create a route
+// Home view
 app.get('/', (req, res) => {
     res.render('index');
 });
 
+// Cards view
 app.get('/cards', (req, res) => {
     res.render('cards', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose thomb it is."});
+});
+
+// Hello view
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+app.post('/hello', (req, res) => {
+    res.render('hello', ({ name: req.body.username }));
 });
 
 // Sets up development server
